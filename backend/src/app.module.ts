74d5e5abaf7f86435d78d840f8eff8af29887387
umar-abdullah22 <umar.coderzhunt@gmail.com>
2023-05-21 15:39:50 +0500

    modified db options

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,32 +2,37 @@ import { Module } from '@nestjs/common';
 
 // config imports
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import {
+  TypeOrmModule,
+  TypeOrmModuleAsyncOptions,
+  TypeOrmModuleOptions,
+} from '@nestjs/typeorm';
 import { ConfigEnum } from '@lib/types';
+import * as ORMConfig from './config/orm.config';
 
 // config imports files
-import typeormConfig from './config/orm.config';
 import serverConfig from './config/server.config';
 import swaggerConfig from './config/swagger.config';
 
 // Module imports
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
-
+export const typeOrmConfig: TypeOrmModuleOptions = ORMConfig;
+export const TypeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: async (): Promise<TypeOrmModuleOptions> => {
+    return ORMConfig;
+  },
+};
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [typeormConfig, serverConfig, swaggerConfig],
+      load: [serverConfig, swaggerConfig],
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) =>
-        await configService.get<Promise<TypeOrmModuleOptions>>(
-          ConfigEnum.TYPEORM,
-        ),
-      inject: [ConfigService],
-    }),
+    TypeOrmModule.forRootAsync(TypeOrmAsyncConfig),
+
     AuthModule,
     UserModule,
   ],
--- backend/src/config/orm.config.ts
+++ backend/src/config/orm.config.ts
@@ -1,28 +1,32 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { resolve } from 'path';
-import { registerAs } from '@nestjs/config';
 import { DataSourceOptions } from 'typeorm';
-import { ConfigEnum } from '@lib/types';
-
 // default directories
 const ENTITIES_DIR = resolve(
   __dirname,
   '../',
   '**',
   'entities',
-  '*.entity.{ts,js}',
+  '*.entity.{ts,js}'
 );
 
-export default registerAs(
-  ConfigEnum.TYPEORM,
-  (): DataSourceOptions => ({
-    type: 'mysql',
-    host: process.env.DB_HOST || 'localhost',
-    port: +process.env.DB_PORT || 3307,
-    username: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD || 'password',
-    database: process.env.DB_NAME || 'fcit',
-    synchronize: true,
-    // logging: true,
-    entities: [ENTITIES_DIR],
-  }),
+const MIGRATIONS_DIR = resolve(
+  __dirname,
+  '../',
+  '**',
+  'migrations',
+  '*.{ts,js}'
 );
+const ORMConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: process.env.DB_HOST || 'localhost',
+  port: +process.env.DB_PORT || 3306,
+  username: process.env.DB_USERNAME || 'root',
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_DATABASE || 'fcit',
+  synchronize: true,
+  entities: [ENTITIES_DIR],
+  autoLoadEntities: true,
+  migrations: [MIGRATIONS_DIR],
+};
+module.exports = ORMConfig;
